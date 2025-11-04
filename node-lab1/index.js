fb5f6bae093dd671f5d70e03e8e09f8f7072d9d5
Lyraa-05 <20106693@mail.wit.ie>
2025-11-04 11:50:04 +0000

    Created simple HTTP server

---
+++ node-lab1/index.js
@@ -0,0 +1,17 @@
+//Load http module to create a http server
+import http from 'http';
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const port = process.env.PORT
+//configure http server to respond with hello world to all requests
+const server = http.createServer((req,res) => {
+    res.writeHead(200);
+    res.end("Hello WAD2!");
+});
+
+server.listen(port);
+
+//put message on the terminal
+console.log(`Server running at ${port}`);
