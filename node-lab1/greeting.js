4b99e4eb42e46eb0f6c7650735fbb96af0ba99eb
Lyraa-05 <20106693@mail.wit.ie>
2025-11-04 12:15:03 +0000

    Completed node lab 1, added extra language greetings for server.

--- node-lab1/greeting.js
+++ node-lab1/greeting.js
@@ -1,6 +1,9 @@
 const greeting = {
     en: "Hello World!",
-    fr: "Bonjour!"
+    fr: "Bonjour!",
+    pl: "Dzień Dobry :D",
+    ga: "Dia Duit!",
+    cs: "Ahoj světe!"
 };
 
 export default greeting;
