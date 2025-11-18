6d81ff2472ee248b537cc860592a1e8665954daf
Lyraa-05 <20106693@mail.wit.ie>
2025-11-18 12:59:21 +0000

    Mongo lab Error HAndling

--- tasky-api/api/tasks/taskModel.js
+++ tasky-api/api/tasks/taskModel.js
@@ -3,13 +3,18 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const TaskSchema = new Schema({
-  title:  String,
+  title: {type: String, required: true},
   description:  String ,
   deadline: Date,
   done: Boolean,
-  priority: {type: String, enum: ["Low","Medium","High"]},
+  priority: {type: String, enum: ["Low","Medium","High"], required: true},
   created_at: {type: Date, default: Date.now},
   updated_at: {type: Date, default: Date.now}
 });
+
+const dateValidator = (date) => {
+  return date > new Date();
+}
+TaskSchema.path("deadline").validate(dateValidator);
 
 export default mongoose.model('Task', TaskSchema);
