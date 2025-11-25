fb3181834f0362af0be3ad14cafc1eec9fd26d6d
Lyraa-05 <20106693@mail.wit.ie>
2025-11-25 12:23:12 +0000

    Added userId to tasks

--- tasky-api/api/tasks/taskModel.js
+++ tasky-api/api/tasks/taskModel.js
@@ -9,7 +9,11 @@ const TaskSchema = new Schema({
   done: Boolean,
   priority: {type: String, enum: ["Low","Medium","High"], required: true},
   created_at: {type: Date, default: Date.now},
-  updated_at: {type: Date, default: Date.now}
+  updated_at: {type: Date, default: Date.now},
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+},
 });
 
 const dateValidator = (date) => {
